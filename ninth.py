87f4238ac94236266293ae02501302d57fed2885
Raima Adhikary <raimaadhikary4@gmail.com>
2025-05-02 12:44:01 -0500

    Add files via upload

---
+++ ninth.py
@@ -0,0 +1,7 @@
+#WAP to input two numbers and print true if first>=second and false if not
+a=int(input("enter first: "))
+b=int(input("enter second: "))
+if(a>=b):
+    print(a>=b)
+else:
+    print(a<b)

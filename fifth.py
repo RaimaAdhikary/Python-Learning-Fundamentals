84fb38d5f9d1c72172bfde3b70a13df0d092c590
Raima Adhikary <raimaadhikary4@gmail.com>
2025-05-02 11:54:51 -0500

    Add files via upload

---
+++ fifth.py
@@ -0,0 +1,33 @@
+#Relational Operators
+# ==, !=, >, <, >=, <=
+A=5
+B=5
+C=4
+D=6
+print(A==B)
+print(A!=B)
+print(A==C)
+print(A<C)
+print(A<=B)
+print(D>A)
+print(D>=B)
+
+#Assignment Operators
+# =, +=, -=,*=,/=, %=,**=
+M=10
+M=M+10
+print(M)
+M+=10
+print(M)
+M-=10
+print(M)
+N=4
+N*=2
+print(N)
+N**=2
+print(N)
+N/=2
+print(N)
+N%=2
+print(N)
+

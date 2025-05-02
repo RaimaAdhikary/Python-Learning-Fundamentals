d5d347f33942cff31b5a065d3e2a72266ffeff45
Raima Adhikary <raimaadhikary4@gmail.com>
2025-05-02 12:31:46 -0500

    Add files via upload

---
+++ eighth.py
@@ -0,0 +1,10 @@
+#WAP to input side of square and print its area
+side=float(input("enter the side of a square: "))
+area=int(side*side)
+print(area)
+#WAP to input float numbers and average it
+
+s=float(input("First: "))
+v=float(input("Second: "))
+avg=(s+v)/2
+print(avg)

294036997a324db9ced57418b7fdfb9e70fa5064
Raima Adhikary <raimaadhikary4@gmail.com>
2025-05-02 12:25:18 -0500

    Add files via upload

---
+++ seventh.py
@@ -0,0 +1,11 @@
+#WAP to input two numbers and print the operations of arithmetic operators
+val1=int(input("enter the first number"))
+val2=int(input("enter the second number"))
+sum=val1+val2
+difference= val1-val2
+product=val1*val2
+Quotient=val1/val2
+print(sum)
+print(difference)
+print(product)
+print(Quotient)

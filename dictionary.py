176a4275740768fadbe0ce79330fb91c3271a74d
Raima Adhikary <raimaadhikary4@gmail.com>
2025-05-17 20:10:30 -0500

    dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -24,3 +24,31 @@ null_info["Subjects"]["Math"]=84
 print(null_info)
 print(null_info["Subjects"])
 print(type(null_info["Subjects"]))
+print(null_info.keys()) # keys is a method and it doesnot fetch inner dicts
+print(info.keys())
+print(len(info))
+print(len(info.keys()))
+print(info.values())## values is a method and it returns only the values
+#type casting changing the dictionary into a list
+
+print(list(info.values()))
+
+print(info.items())
+print(list(info.items()))
+pairs=list(info.items())
+print(pairs[0],pairs[1])
+#get("Key") doesnot disrupt the whole code since it returns something nonetheless
+print(info["City"])
+#print(info["City2"]) cause an error
+print(info.get("City"))
+print(info.get("City2"))
+
+#update() 
+
+new_dict={
+    "City":"Wisconsin",#overwrite
+    "Zipcode":5679,
+    "Country":"Madison"
+}
+info.update(new_dict)
+print(info)
